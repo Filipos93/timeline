e18555ad038cf85433d6f5845b23bb07df5b0587
Filip Marzec <filipm93@gmail.com>
2017-06-23 22:11:50 +0200

    added tooltip

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 $(function(){
 
-	var array = [
-		[new Date(2015, 6, 5), "Event 1", "fa-heart"],
-		[new Date(2015, 6, 15), "Event 3", "fa-heart"],
+	var eventArray = [
+		[new Date(2015, 6, 15), "lorem ipsum dolor sit", "fa-heart"],
 		[new Date(2015, 6, 16), "Event 3", "fa-heart"],
 		[new Date(2015, 6, 3), "Event 2", "fa-heart"],
 		[new Date(2015, 6, 1), "Event 2", "fa-heart"],
-		[new Date(2015, 6, 29), "Event 2", "fa-heart"],
-		[new Date(2015, 6, 30), "Event 2", "fa-heart"]
+		[new Date(2015, 6, 30), "Event 2", "fa-heart"],
+		[new Date(2015, 7, 30), "Event 2", "fa-heart"]
 
 	];
 
 	var today = new Date(2015, 6, 10);
 	var actualDay = today.getDate();
+	var actualMonth = today.getMonth();
 	var barLength = $('#timeline').width();
 	var oneDay = barLength / 30;
 
@@ -20,23 +20,35 @@ $(function(){
 
 	function appendEvents(array){
 		for(var i=0; i<array.length; i++){
-			var dayOfMonth = array[i][0].getDate();
-			$eventToAppend = $('<div class="event"><span class="fa ' + array[i][2] + '"></span></div>');
-			
-			if(actualDay >= dayOfMonth){
-				$eventToAppend.addClass('past');
-			}
+			if(array[i][0].getMonth()==actualMonth){
+				var eventDate = array[i][0];
+				var dayOfMonth = eventDate.getDate();
+				var dayToPrint = dayOfMonth < 10 ? '0' + dayOfMonth : dayOfMonth;
 
-			$eventToAppend.appendTo('#timeline');
+				$eventToAppend = $('<div class="event"><span class="fa ' + array[i][2] + '"></span><div class="tooltip"><p class="date">' + dayToPrint + '.' + eventDate.getMonth() + '.' + eventDate.getFullYear() + '</p><p class="name">' + array[i][1] + '</p></div></div>');
+				
+				if(actualDay >= dayOfMonth){
+					$eventToAppend.addClass('past');
+				}
 
-			if(dayOfMonth==1){
-				$eventToAppend.css("left", -Math.abs($eventToAppend.width()/2));
-			}
-			else{
-				$eventToAppend.css("left", (oneDay * dayOfMonth) - ($eventToAppend.width()/2) );
+				$eventToAppend.appendTo('#timeline');
+
+				if(dayOfMonth==1){
+					$eventToAppend.css("left", -Math.abs($eventToAppend.width()/2));
+				}
+				else{
+					$eventToAppend.css("left", (oneDay * dayOfMonth) - ($eventToAppend.width()/2) );
+				}
 			}
 		}
 	}
 	
-	appendEvents(array);
+	appendEvents(eventArray);
+
+	$('.event').hover(function(){
+		$(this).children('.tooltip').fadeIn(200);
+	}, function(){
+		$(this).children('.tooltip').fadeOut(200);
+	});
+
 });
